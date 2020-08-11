70a5ef72fca47f2516529bf800fa8c9ad61149c9
John Hartmann <johnhartmann26@gmail.com>
2020-08-10 20:14:20 -0500

    implemented unstyled react-table-view

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,30 +1,12 @@
 import React from "react";
+import TableView from "react-table-view";
 
 function Table(props) {
-  let counter = 1;
-  const listItems = props.rankedTeams.map(teamObject => (
-    <tr key={teamObject.name}>
-      <th>{counter++}</th>
-      <th>{teamObject.name.toUpperCase()}</th>
-      <th>{teamObject.points}</th>
-      <th>
-        {teamObject.appearances}/{props.rankersCount}
-      </th>
-    </tr>
-  ));
-  return (
-    <table className="rankingsTable">
-      <tbody>
-        <tr>
-          <th>Rank</th>
-          <th>Team</th>
-          <th>Points</th>
-          <th>Top 20</th>
-        </tr>
-        {listItems}
-      </tbody>
-    </table>
-  );
+  if (props.rankedTeams.length > 0) {
+    return <TableView data={props.rankedTeams} />;
+  } else {
+    return <p>Bear with us...</p>;
+  }
 }
 
 export default Table;
